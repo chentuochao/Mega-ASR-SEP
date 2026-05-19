db804a75c694d8c6db850386c0575d05f0eb817e
zhifei <1454019631@qq.com>
2026-05-19 18:44:13 +0800

    simple

---
+++ src/MegaASR/model/megaASR.py
@@ -0,0 +1,17 @@
+Class Mega_ASR():
+
+    def Download
+        snapload(huggingface) 下载
+
+
+    def infer with lora:
+
+        pass
+
+    def infer_without_lora:
+        pass
+
+
+
+def  get_Mega_ASR():
+    return Mega_ASR()
